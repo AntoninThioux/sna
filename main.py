740d16e373d11e36c5ce87ec567f6f54e1b3a444
AntoninThioux <antonin.thioux@gmail.com>
2022-10-21 16:47:58 +0200

    Added code for community analysis
    Co-authored-by: MrNiekio <MrNiekio@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from html import entities
 from re import sub
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import network_loader as loader
+from networkx.algorithms.community.centrality import girvan_newman
 
 
 PATH = "./charliehebdo-all-rnr-threads/"
@@ -10,8 +12,9 @@ PATH = "./charliehebdo-all-rnr-threads/"
 
 def main():
     G = loader.load_rnr_graph(PATH)
-    print_metrices(G)
-    print_communities(G)
+    # print_metrices(G)
+    # print_communities(G)
+    show_plots(G)
 
 
 # PART 1
@@ -57,6 +60,74 @@ def print_communities(G):
     cliques = list(nx.enumerate_all_cliques(G))
     number_of_cliques = [len(c) for c in cliques if len(c) >= 3]
     print(f"Number of cliques (>3) = {len(number_of_cliques)}")
+
+    cross = 0
+    for (n0, n1) in G.edges():
+        if G.nodes[n0]['is_spreading_rumours'] and not G.nodes[n1]['is_spreading_rumours']:
+            cross += 1
+    r = nr = 0
+    for n in G.nodes():
+        if G.nodes[n]['is_spreading_rumours']:
+            r += 1
+        else:
+            nr += 1
+    print(f"Homophily cross-(non)rumour = {cross / len(G.edges):.4f}")
+    print(f'\t 2qp = {r * nr / len(G.nodes()) / len(G.nodes()):.4f}')
+        
+
+    homophily = nx.numeric_assortativity_coefficient(G, 'is_spreading_rumours')
+    print(f'homophily = {homophily}')
+
+    bridges = nx.bridges(G)
+    # look at bridges with high centarilty or something
+    print(f'bridges = {len(list(bridges))}')
+
+    """
+    generater = girvan_newman(G)
+    next(generater)
+    print("Test")
+    communities = [[G.nodes[n]['is_spreading_rumours'] for n in c] for c in next(generater)]
+    print(f"number of communities = {len(communities)}")
+    l = [c.count(True) / len(c) for c in communities]
+    for i in range(5):
+        print(f"{l[i]:2f}", end='')
+    print()
+    original_len = len(communities)
+    while (len(communities) < 3 * original_len):
+        communities = [[G.nodes[n]['is_spreading_rumours'] for n in c] for c in next(generater)] 
+        l = [c.count(True) / len(c) for c in communities]
+        print(f"number of communities = {len(communities)}")
+        for i in range(5):
+            print(f"{l[i]:2f}", end='')
+        print()    
+    """
+
+    D = loader.load_rnr_graph_directed(PATH)
+    hubs, authorities = nx.hits(D)
+
+    max_hub = max(hubs, key=hubs.get)
+    max_authority = max(authorities, key=authorities.get)
+
+    print(f'Max hub node = {max_hub} with value of {hubs[max_hub]}')
+    print(f'{D.nodes[max_hub]}')
+    print(f'Max authority node = {max_authority} with value of {authorities[max_authority]}')
+    print(f'{D.nodes[max_authority]}')
+
+    print(f'partitioning')
+    
+
+def show_plots(G):
+    # Degree distribution 
+    degrees = [d for n,d in G.degree()]
+    plt.hist(degrees)
+    plt.yscale('log')
+    plt.xscale()
+    plt.show()
+    
+    # Centrality distribution
+    number_of_points = nx.number_of_nodes(G)
+    
+
 
 
 #print(G.nodes)
--- network_loader.py
+++ network_loader.py
@@ -13,6 +13,12 @@ def main():
     print('Trying to load graph...')
     G = load_rnr_graph(PATH)
     print('Rumours/non-rumour graph loaded successfully!')
+    i = 0
+    for n in G.nodes:
+        if i > 20:
+            break
+        print(G.nodes[n])
+        i += 1
 
 
 """
@@ -22,21 +28,49 @@ def load_tweet(tid, G, path):
     f = open(path + f'{tid}/source-tweets/{tid}.json')
     tweet = json.load(f)
     f.close()
+    G.add_node(tweet['user']['id'])
     G.add_edges_from([(tweet['user']['id'], mention['id']) for mention in tweet['entities']['user_mentions']])
+    return tweet['user']['id']
+
+
+def load_tweet_annotations(tid, G, path, nid):
+    f = open(path + f'{tid}/annotation.json')
+    annotation = json.load(f)
+    f.close()
+
+    if (annotation['is_rumour'] == 'rumour'):
+        G.nodes[nid]['rumour_count'] = G.nodes[nid].get('rumour_count', 0) + 1
+    else:
+        G.nodes[nid]['non-rumour_count'] = G.nodes[nid].get('non-rumour_count', 0) + 1
 
 
 """
 This function loads the reactions to a tweet in the dataset.
 """
 def load_reactions(tid, G, path):
+    r_ids = []
     files = os.listdir(path + f'{tid}/reactions')
     for reaction_file in files:
         rf = open(path + f'{tid}/reactions/' + reaction_file)
         subtweet = json.load(rf)
         G.add_edges_from([(subtweet['user']['id'], mention['id']) for mention in subtweet['entities']['user_mentions']])
-            
+        r_ids.append(subtweet['user']['id']) 
+
         rf.close()
 
+    return r_ids
+
+
+def guilt_by_reaction(tid, G, path, rn_ids):
+    f = open(path + f'{tid}/annotation.json')
+    annotation = json.load(f)
+    f.close()
+
+    for rid in rn_ids:
+        if (annotation['is_rumour'] == 'rumour'):
+            G.nodes[rid]['rumour_count'] = G.nodes[rid].get('rumour_count', 0) + 1
+        else:
+            G.nodes[rid]['non-rumour_count'] = G.nodes[rid].get('non-rumour_count', 0) + 1
 
 """
 This function loads the tweets in a directory.
@@ -46,14 +80,26 @@ def load_directory(G, dir):
 
     ids = os.listdir(dir)
     for tweet_id in ids:
-        load_tweet(tweet_id, G, dir)
-        load_reactions(tweet_id, G, dir)
+        node_id = load_tweet(tweet_id, G, dir)
+        rnode_ids = load_reactions(tweet_id, G, dir)
+        load_tweet_annotations(tweet_id, G, dir, node_id)
+        guilt_by_reaction(tweet_id, G, dir, rnode_ids)
         x += 1
         print(f'\rLoaded {x / len(ids) * 100:.1f}% tweets from {dir}',end="")
     
     print(f'\rLoaded 100.0% of tweets from {dir}')
-    
 
+
+"""
+This function classifies all nodes in the graph as rumour-spreaders and non-rumour-spreaders
+"""
+def classify(G):
+    for n in G.nodes:
+        rc = G.nodes[n].get('rumour_count', 0)
+        nrc = G.nodes[n].get('non-rumour_count', 0)
+        G.nodes[n]['is_spreading_rumours'] = (not rc < nrc)
+        
+    
 """
 This function loads the graph.
 """
@@ -61,8 +107,16 @@ def load_rnr_graph(path):
     G = nx.Graph()
     load_directory(G, path + "rumours/")
     load_directory(G, path + "non-rumours/")
+    classify(G)
     return G
 
+
+def load_rnr_graph_directed(path):
+    G = nx.DiGraph()
+    load_directory(G, path + "rumours/")
+    load_directory(G, path + "non-rumours/")
+    classify(G)
+    return G
 
 if __name__ == "__main__":
     main()
